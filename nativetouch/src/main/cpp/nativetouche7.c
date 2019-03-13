bd03d3e92d0a10e5985faef88ce9ff0442597130
5argon <pyasry@gmail.com>
2019-03-13 16:15:29 +0700

    Updated for the new ring buffer based approach

--- nativetouch/src/main/cpp/nativetouche7.c
+++ nativetouch/src/main/cpp/nativetouche7.c
@@ -3,12 +3,20 @@
 //
 
 // Cannot do extern in Unity if this is .cpp file lol why
+
 // Note : Using AndroidJavaProxy to talk back to Unity is not thread safe and will randomly cause SIGSEGV 11.
 // when triggered from other thread like Android's touch.
 // Using native delegate like you see here is safe.
 
+// This file is compiled to dynamically linked library named "libnativetouche7" according to the CMakeList.txt file.
+// It could be used from Unity with [DllImport("nativetouche7")]
+// Note that Android could not do static linking like iOS. (If that is possible then we could  [DllImport("__Internal")]
+
 #include <jni.h>
 
+//CAREFULLY design the struct shape to be the same as in C#.
+//We are writing to C# memory here! And that includes,
+//we have to make space for even fields that we aren't using on Android. (But used in iOS)
 typedef struct
 {
     int callbackType;
@@ -46,61 +54,99 @@ typedef struct
     float orientation;
 } NativeTouchDataFull;
 
-typedef void (*NativeTouchFullDelegate)(NativeTouchDataFull datas);
-typedef void (*NativeTouchMinimalDelegate)(NativeTouchData datas);
+//This type of delegate tells C# to check the ring buffer starting from `start` index and goes by `count`.
+//That's all the new touches.
+typedef void (*NativeTouchCheckRingBufferDelegate)(int start, int count);
 
-NativeTouchFullDelegate fullCallback;
-NativeTouchMinimalDelegate minimalCallback;;
+int ringBufferSize = -1;
+int startingMinimalRbIndex = 0;
+int minimalRbCurrentCount = 0;
+int startingFullRbIndex = 0;
+int fullRbCurrentCount = 0;
 
-void registerCallbacks(NativeTouchFullDelegate fullDelegate, NativeTouchMinimalDelegate minimalDelegate)
+NativeTouchCheckRingBufferDelegate fullCallbackCheckRingBuffer;
+NativeTouchCheckRingBufferDelegate minimalCallbackCheckRingBuffer;
+//Receive ring buffer space from C#, write on native side.
+NativeTouchData* ntdRingBuffer;
+NativeTouchDataFull* ntdFullRingBuffer;
+
+void registerCallbacksCheckRingBuffer(NativeTouchCheckRingBufferDelegate fullDelegate, NativeTouchCheckRingBufferDelegate minimalDelegate,
+NativeTouchDataFull* fullRingBuffer, NativeTouchData* minimalRingBuffer, int ringBufferSizeFromCSharp )
 {
-    fullCallback = fullDelegate;
-    minimalCallback = minimalDelegate;
+    //You could increase the ring buffer size from a `const` in C#. It would be synchronized to here.
+    ringBufferSize = ringBufferSizeFromCSharp;
+
+    fullCallbackCheckRingBuffer = fullDelegate;
+    minimalCallbackCheckRingBuffer = minimalDelegate;
+
+    ntdFullRingBuffer = fullRingBuffer;
+    ntdRingBuffer = minimalRingBuffer;
+}
+void Java_com_Exceed7_NativeTouch_NativeTouchListener_startTouches(JNIEnv *env, jclass clazz)
+{
+    minimalRbCurrentCount = 0;
+    fullRbCurrentCount = 0;
 }
 
-void Java_com_Exceed7_NativeTouch_NativeTouchListener_sendTouchMinimal(JNIEnv *env, jclass clazz,
+void Java_com_Exceed7_NativeTouch_NativeTouchListener_writeTouchMinimal(JNIEnv *env, jclass clazz,
         int callbackType, float x, float y, int phase, double timestamp, int pointerId)
 {
-    NativeTouchData ntd;
+    //Try to write C# memory in-place so we don't have to allocate the struct needlessly.
+    //We have already paid JNI cost from Java to C with tons of args...
+    NativeTouchData* ntd = &ntdRingBuffer[(startingMinimalRbIndex + minimalRbCurrentCount) % ringBufferSize];
 
-    ntd.callbackType = callbackType;
-    ntd.x = x;
-    ntd.y = y;
-    ntd.previousX = -1; //no use on android
-    ntd.previousY = -1; //no use on android
-    ntd.phase = phase;
-    ntd.timestamp = timestamp;
-    ntd.pointerId = pointerId;
-    ntd.nativelyGenerated = 1;
+    ntd->callbackType = callbackType;
+    ntd->x = x;
+    ntd->y = y;
+    ntd->previousX = -1; //no use on android
+    ntd->previousY = -1; //no use on android
+    ntd->phase = phase;
+    ntd->timestamp = timestamp;
+    ntd->pointerId = pointerId;
+    ntd->nativelyGenerated = 1;
 
-    minimalCallback(ntd);
+    minimalRbCurrentCount++;
 }
 
-void Java_com_Exceed7_NativeTouch_NativeTouchListener_sendTouchFull(JNIEnv *env, jclass clazz,
+
+void Java_com_Exceed7_NativeTouch_NativeTouchListener_writeTouchFull(JNIEnv *env, jclass clazz,
                 int callbackType, float x, float y,  int phase, double timestamp, int pointerId,
                 float orientation, float pressure,float size, float touchMajor, float touchMinor)
 {
-    NativeTouchDataFull ntd;
+    //Try to write C# memory in-place so we don't have to allocate the struct needlessly.
+    //We have already paid JNI cost from Java to C with tons of args...
+    NativeTouchDataFull* ntd = &ntdFullRingBuffer[(startingFullRbIndex + fullRbCurrentCount) % ringBufferSize];
 
-    ntd.callbackType = callbackType;
-    ntd.x = x;
-    ntd.y = y;
-    ntd.previousX = -1; //no use on android
-    ntd.previousY = -1; //no use on android
-    ntd.phase = phase;
-    ntd.timestamp = timestamp;
-    ntd.pointerId = pointerId;
-    ntd.nativelyGenerated = 1;
+    ntd->callbackType = callbackType;
+    ntd->x = x;
+    ntd->y = y;
+    ntd->previousX = -1; //no use on android
+    ntd->previousY = -1; //no use on android
+    ntd->phase = phase;
+    ntd->timestamp = timestamp;
+    ntd->pointerId = pointerId;
+    ntd->nativelyGenerated = 1;
 
-    ntd.tapCount = -1; //no use on android
-    ntd.type = -1; //no use on android
-    ntd.pressure = pressure;
-    ntd.maximumPossibleForce = -1; //no use on android
-    ntd.touchMajor = touchMajor;
-    ntd.touchMinor = touchMinor;
-    ntd.size = size;
-    ntd.orientation = orientation;
+    ntd->tapCount = -1; //no use on android
+    ntd->type = -1; //no use on android
+    ntd->pressure = pressure;
+    ntd->maximumPossibleForce = -1; //no use on android
+    ntd->touchMajor = touchMajor;
+    ntd->touchMinor = touchMinor;
+    ntd->size = size;
+    ntd->orientation = orientation;
 
-    fullCallback(ntd);
+    fullRbCurrentCount++;
+}
 
+void Java_com_Exceed7_NativeTouch_NativeTouchListener_commitTouchesMinimal(JNIEnv *env, jclass clazz)
+{
+    minimalCallbackCheckRingBuffer(startingMinimalRbIndex, minimalRbCurrentCount);
+    startingMinimalRbIndex = (startingMinimalRbIndex + minimalRbCurrentCount) % ringBufferSize;
+}
+
+void Java_com_Exceed7_NativeTouch_NativeTouchListener_commitTouchesFull(JNIEnv *env, jclass clazz)
+{
+    fullCallbackCheckRingBuffer(startingFullRbIndex, fullRbCurrentCount);
+    startingFullRbIndex = (startingFullRbIndex + fullRbCurrentCount) % ringBufferSize;
 }
--- nativetouch/src/main/java/com/Exceed7/NativeTouch/NativeTouchListener.java
+++ nativetouch/src/main/java/com/Exceed7/NativeTouch/NativeTouchListener.java
@@ -12,111 +12,173 @@ public class NativeTouchListener {
     static View.OnTouchListener nativeTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent event) {
-            //Log.i("DISPATCH ", event.getX() + " " + event.getY() + " Time : " + String.valueOf(event.getEventTime()) +
-            //" Action : " + event.getActionMasked() + " Pointer count : " + event.getPointerCount());
 
-            //Report the same timestamp for all touch even if they move or not.
-            double timestamp = (double)AndroidTouchTime();
+            //Report the same timestamp for all touches, even if they move or not. We have to quickly do this the first thing on the listener.
+            double listenerTimestamp = (double) AndroidTouchTime();
 
-            //MotionEvent might contains multiple touch data. We will callback once for each.
+            startTouches();
 
-                int pointerCount = event.getPointerCount();
-                int act = event.getActionMasked();
-                boolean downAction = act == MotionEvent.ACTION_DOWN || act == MotionEvent.ACTION_POINTER_DOWN;
-                boolean upAction = act == MotionEvent.ACTION_UP || act == MotionEvent.ACTION_POINTER_UP;
-                boolean moveAction = act == MotionEvent.ACTION_MOVE;
-                boolean cancelledAction = act == MotionEvent.ACTION_CANCEL;
+            Log.i("DISPATCH ", event.getX() + " " + event.getY() + " Android Touch Time : " + String.valueOf(listenerTimestamp) + " Time : " + String.valueOf(event.getEventTime()) + " Action : " + event.getActionMasked() + " Pointer count : " + event.getPointerCount());
 
-                //Determine the callback type from the "main" action. Used with every touches in this MotionEvent.
-                int callbackType = 4; //cancelled
-                if (downAction) {
-                    callbackType = 0;
-                } else if (upAction) {
-                    callbackType = 3;
-                } else if (moveAction) {
-                    callbackType = 1;
+            //One `MotionEvent` contains multiple touch data. We will peel them out and write to ring buffer, then told C# to check them.
+
+            int pointerCount = event.getPointerCount();
+
+            //One `MotionEvent` may includes other unrelated fingers, but only one "main event" exist in this. We will determine that main event.
+
+            int act = event.getActionMasked();
+            boolean downAction = false;
+            boolean upAction = false;
+            boolean moveAction = false;
+            boolean cancelledAction = false;
+
+            switch (act) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    downAction = true;
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_POINTER_UP:
+                    upAction = true;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    moveAction = true;
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    cancelledAction = true;
+                    break;
+            }
+
+            //Determine the callback type from the "main" action. Use it with every touches in this `MotionEvent`.
+            int callbackType = 4; // Default to cancelled.
+            if (downAction) {
+                callbackType = 0;
+            } else if (upAction) {
+                callbackType = 3;
+            } else if (moveAction) {
+                callbackType = 1;
+            }
+
+            int pointerIdOfAction = event.getPointerId(event.getActionIndex());
+            for (int i = 0; i < pointerCount; i++) {
+                int pointerId = event.getPointerId(i);
+                boolean actionOccurOnThisPointer = pointerIdOfAction == pointerId;
+
+                float x = event.getX(i);
+                float y = event.getY(i);
+
+                //We are not using the time that comes with the touch.
+                //Because for touches that aren't moving, their time is not updated.
+                //Instead, we bake the time we get manually at the very first line to all touches.
+                //float time = event.getEventTime();
+
+                int phase = 4;
+                if (downAction && actionOccurOnThisPointer) {
+                    phase = MotionEvent.ACTION_DOWN;
+                } else if (upAction && actionOccurOnThisPointer) {
+                    phase = 3; //MotionEvent.ACTION_UP; //UP is 1 in Android but in Unity it's 3
+                } else if (cancelledAction) {
+                    // Cancelled action will put all touches in the pack as Cancelled.
+                    // Does not care about whether if the action is on this touch or not.
+                    phase = 4;
+                } else if (!actionOccurOnThisPointer || moveAction) {
+                    //If action is not on this pointer we are not sure the others move or stay still. We always say move.
+                    //If it is move action then also we don't know which one move or stay still. We always say move.
+                    phase = 1; //MotionEvent.ACTION_MOVE; //MOVE is 2 in Android but in Unity it's 1
                 }
 
-                int pointerIdOfAction = event.getPointerId(event.getActionIndex());
-                for (int i = 0; i < pointerCount; i++) {
-                    int pointerId = event.getPointerId(i);
-                    boolean actionOccurOnThisPointer = pointerIdOfAction == pointerId;
+                if (isMinimalMode) {
+                    //Log.i("EACH", x + " " + y + " Action? " + actionOccurOnThisPointer + " ("+ pointerIdOfAction +") ID " + pointerId );
+                    //Writes the touch to ring buffer, and advance the ring buffer pointer.
+                    writeTouchMinimal(callbackType, x, y, phase, listenerTimestamp, pointerId);
+                } else {
+                    //Get more..
+                    float orientation = event.getOrientation(i);
+                    float pressure = event.getPressure(i);
+                    float size = event.getSize(i);
+                    float touchMajor = event.getTouchMajor(i);
+                    float touchMinor = event.getTouchMinor(i);
 
-                    float x = event.getX(i);
-                    float y = event.getY(i);
-                    int phase = 4;
-                    if (downAction && actionOccurOnThisPointer) {
-                        phase = MotionEvent.ACTION_DOWN;
-                    } else if (upAction && actionOccurOnThisPointer) {
-                        phase = 3; //MotionEvent.ACTION_UP; //UP is 1 in Android but in Unity it's 3
-                    } else if (cancelledAction) {
-                        // Cancelled action will put all touches in the pack as Cancelled.
-                        // Does not care about whether if the action is on this touch or not.
-                        phase = 4;
-                    }else if (!actionOccurOnThisPointer || moveAction) {
-                        //If action is not on this pointer we are not sure the others move or stay still. We always say move.
-                        //If it is move action then also we don't know which one move or stay still. We always say move.
-                        phase = 1; //MotionEvent.ACTION_MOVE; //MOVE is 2 in Android but in Unity it's 1
-                    }
-
-                    if (isMinimalMode) {
-                        //If minimal mode we can start sending now!
-                        //Log.i("EACH", x + " " + y + " Action? " + actionOccurOnThisPointer + " ("+ pointerIdOfAction +") ID " + pointerId );
-                        sendTouchMinimal(callbackType, x, y, phase, timestamp, pointerId);
-                    } else {
-                        //Get more..
-                        float orientation = event.getOrientation(i);
-                        float pressure = event.getPressure(i);
-                        float size = event.getSize(i);
-                        float touchMajor = event.getTouchMajor(i);
-                        float touchMinor = event.getTouchMinor(i);
-                        sendTouchFull(callbackType, x, y, phase, timestamp, pointerId,
-                                orientation, pressure, size, touchMajor, touchMinor);
-                    }
+                    //Writes the touch to ring buffer, and advance the ring buffer pointer.
+                    writeTouchFull(callbackType, x, y, phase, listenerTimestamp, pointerId,
+                            orientation, pressure, size, touchMajor, touchMinor);
                 }
+            }
 
+            //All ring buffer writes are now complete for this listener call.
+            //We now do reverse p-invoke once to tell C# to check the buffer at specific spot.
+            //Those spots are a parameter of that invoke.
+
+            //If we are in no callback mode, we rely on C# to manually read out the ring buffer without telling it now.
+            if(isNoCallback == false) {
+                if (isMinimalMode) {
+                    commitTouchesMinimal();
+                } else {
+                    commitTouchesFull();
+                }
+            }
+
+            //So that everything else like uGUI, EventTrigger, etc. in Unity still works, we are forwarding the touch to Unity also.
+            //However ours will probably arrives first, be it the callback-based way or dequeue-based way.
             if (!isDisableUnityTouch) {
-                //Unity input still works
                 UnityPlayer.currentActivity.onTouchEvent(event);
             }
+
+            //Returning true for successful touch. (When will it could be false??)
             return true;
         }
-
     };
 
     private static boolean isMinimalMode;
     private static boolean isDisableUnityTouch;
+    private static boolean isNoCallback;
 
-    public static void StopNativeTouch()
-    {
+    public static void StopNativeTouch() {
         //Log.i("Android Native Touch","Stopped");
         UnityPlayer.currentActivity.getCurrentFocus().setOnTouchListener(null);
     }
 
-    public static void StartNativeTouch(boolean fullMode, boolean disableUnityTouch) {
+    public static void StartNativeTouch(boolean fullMode, boolean disableUnityTouch, boolean noCallback) {
         //Log.i("Android Native Touch","Registered");
         isMinimalMode = !fullMode;
         isDisableUnityTouch = disableUnityTouch;
+        isNoCallback = noCallback;
         UnityPlayer.currentActivity.getCurrentFocus().setOnTouchListener(nativeTouchListener);
     }
 
-    public static int RealScreenHeight()
-    {
+    public static int RealScreenHeight() {
         return UnityPlayer.currentActivity.getCurrentFocus().getHeight();
     }
 
-    public static int RealScreenWidth()
-    {
+    public static int RealScreenWidth() {
         return UnityPlayer.currentActivity.getCurrentFocus().getWidth();
     }
 
-    public static long AndroidTouchTime()
-    {
+    public static long AndroidTouchTime() {
         return SystemClock.uptimeMillis();
     }
 
+    //This probably has no use, but may be handy for debugging here and also from Unity.
+    public static long ElapsedRealtimeNanos() {
+        return SystemClock.elapsedRealtimeNanos();
+    }
+
+    public static native void dispose();
+
     public static native void sendTouchMinimal(int callbackType, float x, float y, int phase, double timestamp, int pointerId);
-    public static native void sendTouchFull(int callbackType, float x, float y,  int phase, double timestamp, int pointerId,
-                                            float orientation, float pressure,float size, float touchMajor, float touchMinor);
+
+    public static native void sendTouchFull(int callbackType, float x, float y, int phase, double timestamp, int pointerId,
+                                            float orientation, float pressure, float size, float touchMajor, float touchMinor);
+
+    public static native void startTouches();
+
+    public static native void writeTouchMinimal(int callbackType, float x, float y, int phase, double timestamp, int pointerId);
+
+    public static native void writeTouchFull(int callbackType, float x, float y, int phase, double timestamp, int pointerId,
+                                             float orientation, float pressure, float size, float touchMajor, float touchMinor);
+
+    public static native void commitTouchesMinimal();
+
+    public static native void commitTouchesFull();
 
 }
